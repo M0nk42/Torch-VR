f964765fc4070d71d757e2e80ba625e2c4a99824
Daydreel <daydreel@gmail.com>
2019-02-07 10:20:23 +0100

    Soufflet WIP

---
+++ VRWorkshop_GD02/Assets/Scripts/VRTK/JVRTK_SouffletPanelVelocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class JVRTK_SouffletPanelVelocity : MonoBehaviour {
+
+
+    VRTK_VelocityEstimator velEstimator;
+	// Use this for initialization
+	void Start () {
+        velEstimator = GetComponent<VRTK_VelocityEstimator>();
+        velEstimator.StartEstimation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Acceleration" + velEstimator.GetAccelerationEstimate());
+
+        Debug.Log("Angular Velocity" + velEstimator.GetAngularVelocityEstimate());
+
+        Debug.Log("Angular Acceleration" + velEstimator.GetVelocityEstimate());
+    }
+}
